99fb49fd7af9f3d1bb46fd5dc5e029e8ca655c75
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2022-10-10 00:55:48 +0100

    Get rid of waste

--- mix.exs
+++ mix.exs
@@ -14,7 +14,6 @@ defmodule ElixirCiTest.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
     ]
   end
 
--- test/elixir_ci_test_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ElixirCiTestTest do
-  use ExUnit.Case
-  doctest ElixirCiTest
-
-  test "greets the world" do
-    assert ElixirCiTest.hello() == :world
-  end
-end
