9cc4d757babd5ae151943a05d1dea7b3367e9af0
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2022-10-10 01:26:36 +0100

    Add some Rust to it

--- lib/elixir_ci_test.ex
+++ lib/elixir_ci_test.ex
@@ -3,6 +3,11 @@ defmodule ElixirCiTest do
   Documentation for `ElixirCiTest`.
   """
 
+  use Rustler,
+    otp_app: :elixir_ci_test,
+    crate: :elixircitest,
+    mode: :debug
+
   @doc """
   Hello world.
 
@@ -12,6 +17,10 @@ defmodule ElixirCiTest do
       :world
 
   """
+  def add(_left, _right) do
+    :erlang.nif_error(:nif_not_loaded)
+  end
+
   def hello do
     :world
   end
--- mix.exs
+++ mix.exs
@@ -20,7 +20,8 @@ defmodule ElixirCiTest.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 1.2"}
+      {:dialyxir, "~> 1.2"},
+      {:rustler, "~> 0.26.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
